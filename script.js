5e87cdab628bd47d52a049b1c36462ba121efec4
Sieghart <sieghart2005@gmail.com>
2023-10-15 17:49:01 -0500

    ahora si, todo joya

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
-
-
 const capas = ["U", "D", "F", "B", "L", "R"];
 const contrarias = { "U": "D", "D": "U", "F": "B", "B": "F", "L": "R", "R": "L" };
 const variables = ["'", 2, ""];
 const scrambleElement = document.getElementById("scramble")
 
 const Scramble = (capas,contrarias,variables,scrambleElement)=>{
-    let lastMove = [];
-    let lastMoveSingle = "";
+    let lastLayer = "";
     let scramble = [];
-    let contador = 0;
-    for(let i = 0;i<21;i++){
-        let movimiento = capas[Math.floor(Math.random()*capas.length)] + variables[Math.floor(Math.random()*variables.length)]; 
-        if (lastMove == ""){
+    let functionals;
+    for(let i = 0;i<19;i++){
+        let movimiento = capas[Math.floor(Math.random()*capas.length)] + variables[Math.floor(Math.random()*variables.length)];
+        if(scramble == ""){
             scramble.push(movimiento);
-            lastMove.push(movimiento);
-            lastMoveSingle = movimiento;
-            contador++;
-        } else if(contador < 2){
-            if(movimiento[0] == lastMoveSingle[0]){
-                let functionals = capas.filter(move => move.startsWith(!movimiento[0]));
+            lastLayer = movimiento[0];
+        } else if(scramble.length != 3){
+            if(lastLayer == movimiento[0]){
+                functionals = capas.filter(move=> move != lastLayer);   
                 movimiento = functionals[Math.floor(Math.random()*functionals.length)] + variables[Math.floor(Math.random()*variables.length)];
                 scramble.push(movimiento);
-                lastMove.push(movimiento);
-                lastMoveSingle = movimiento;
-                contador++;
+                lastLayer = movimiento[0];
             } else {
                 scramble.push(movimiento);
-                lastMove.push(movimiento);
-                lastMoveSingle = movimiento;
-                contador++;
+                lastLayer = movimiento[0];
             }
-        } else if(contador == 2){
-            for(x=0;x<=2;x++){
-                if(contrarias[lastMove[x][0]] == movimiento){
-                    let functionals = capas.filter(move => move.startsWith(!movimiento[0]));
+        } else {
+            let ultimasTres = scramble.slice(-3);
+            ultimasTres.forEach(e=>{
+                if(contrarias[e[0]] == movimiento[0]){
+                    functionals = capas.filter(move=> move != lastLayer)
+                    console.log(functionals);
                     movimiento = functionals[Math.floor(Math.random()*functionals.length)] + variables[Math.floor(Math.random()*variables.length)];
                     scramble.push(movimiento);
-                    lastMove.push(movimiento);
-                    lastMoveSingle = movimiento;
-                    contador = 0;
-                    lastMove = [];
+                    lastLayer = movimiento[0];
                 } else {
-                    scramble.push(movimiento);
-                    lastMove.push(movimiento);
-                    lastMoveSingle = movimiento;
-                    contador = 0;
-                    lastMove = [];
+                    if(lastLayer == movimiento[0]){
+                        functionals = capas.filter(move=> move != lastLayer);   
+                        movimiento = functionals[Math.floor(Math.random()*functionals.length)] + variables[Math.floor(Math.random()*variables.length)];
+                        scramble.push(movimiento);
+                        lastLayer = movimiento[0];
+                    } else {
+                        scramble.push(movimiento);
+                        lastLayer = movimiento[0];
+                    }
                 }
-            }
+            })
         }
-        scramble.forEach(e=>{
-            scrambleElement.innerHTML += ` ${e} `
-        })
     }
+    scramble.forEach(e=>{
+        scrambleElement.innerHTML += ` ${e} `;
+    })
 }
 
 Scramble(capas,contrarias,variables,scrambleElement);
